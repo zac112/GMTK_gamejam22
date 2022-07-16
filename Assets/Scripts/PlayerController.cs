208526f650dbfa6eeb7ab7ccb88dfb4f065984ac
Erno Lokkila <eolokk@utu.fi>
2022-07-16 14:07:04 +0300

    Added trees and modified movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float wingPower = 1f;
     public float moveSpeed = 1f;
     public float turnSpeed = 1f;
+    public float walkSpeed = 0.1f;
+    public bool walking = true;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +20,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print(rb.velocity);
         if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.AddForce(Vector3.up* wingPower);
@@ -26,21 +27,66 @@ public class PlayerController : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.W))
         {
-            rb.AddTorque(-transform.right * turnSpeed);
+            transform.Rotate(-Vector3.right, turnSpeed);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            rb.AddTorque(transform.right * turnSpeed);
+            transform.Rotate(Vector3.right, turnSpeed);
+            //rb.AddTorque(transform.right * turnSpeed);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            rb.AddRelativeTorque(-transform.up * turnSpeed);
+            transform.Rotate(-transform.up, turnSpeed);
+            //transform.Rotate(-Vector3.up, turnSpeed);
+            //rb.AddRelativeTorque(-transform.up * turnSpeed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            //transform.Rotate(transform.up, turnSpeed);
-            rb.AddRelativeTorque(transform.up*turnSpeed);
+            transform.Rotate(transform.up, turnSpeed);
+            //transform.Rotate(Vector3.up, turnSpeed);
+            //rb.AddRelativeTorque(transform.up*turnSpeed);
             //rb.AddTorque(-transform.right * turnSpeed);
         }
+        if (IsWalking())
+        {
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                transform.position += transform.forward * walkSpeed;
+            }
+        }
+    }
+
+    private bool IsWalking()
+    {
+        return walking;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Bug")) CatchBug(collision.gameObject);
+        if (collision.gameObject.CompareTag("Terrain")) StartWalking();
+        if (collision.gameObject.CompareTag("Tree")) StartWalking();
+
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Terrain")) StopWalking();
+        if (collision.gameObject.CompareTag("Tree")) StopWalking();
+
+    }
+    private void StartWalking()
+    {
+        transform.up = Vector3.up;
+        walking = true;
+    }
+    private void StopWalking()
+    {
+        walking = false;
+    }
+
+    private void CatchBug(GameObject bug)
+    {
+        print("Caught bug");
     }
 }
